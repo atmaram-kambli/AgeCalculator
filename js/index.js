890009e0981eef09d09b3f93a9733ccb55a85891
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2023-08-23 23:04:00 +0530

    finalize UI

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
-const dish1 = document.querySelectorAll(".dis");
-console.log(dish1);
+const yyyy = document.getElementById("yyyy");
+console.log(yyyy);
+const mm = document.querySelector("#mm");
+const dd = document.querySelector("#dd");
+function setColor() {
+    yyyy.style.color = "blue";
+    mm.style.color = "blue";
+    dd.style.color = "blue";
+}
+addEventListener('load', setColor);
